c929410423341469692a790ecc48197307f61898
VipLab NCNU <csie@ip232.puli23-21-10.ncnu.edu.tw>
2018-04-22 15:36:49 +0800

    Initial Commit

---
+++ Buttterfly_Illustration_105321512/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buttterfly_Illustration_105321512
+//
+//  Created by VipLab NCNU on 2018/4/22.
+//  Copyright © 2018年 VipLab NCNU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
